2dec9ba81d4ce35ccb9f2aa4eecb796c71dee1c2
GonzaloBouso <gonzalobouso002@gmail.com>
2025-10-08 02:37:13 -0300

    add contenido a páginas para build

---
+++ src/app/(pages)/contacto/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactoPage(){
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-center p24">
+        <h1 className="text-4xl font-bold">Pagina de contacto</h1>
+        <p>Contenido Proximamente...</p>
+        </main>
+    );
+}
---
+++ src/app/(pages)/servicios/page.tsx
@@ -0,0 +1,8 @@
+export default function ServiciosPage(){
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-center p24">
+        <h1 className="text-4xl font-bold">Pagina de servicios</h1>
+        <p>Contenido Proximamente...</p>
+        </main>
+    );
+}
