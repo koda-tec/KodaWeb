745f3b541ad2c570fe36f21729f98fbb39d06c1f
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-10 02:38:53 -0300

    Revert "fix page + layout + ContactFORM"
    This reverts commit ac66c18e34e431b4563ebb5d263052c49c1b1e9b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import HeroSection from './components/sections/HeroSection';
 import ContactSection from './components/sections/ContactoSections';
 import AboutSection from './components/sections/AboutSection';
-import ServicesSection from './components/sections/ServicesSection';
+
 
 export default function Home() {
   return (
     
-    <>
+    <main className="p-8"> 
     <HeroSection/>
     <AboutSection/>
-    <ServicesSection/>
       <ContactSection/>
-    </>
-    
+
+    </main>
   );
 }
