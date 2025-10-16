e4eddc8765968b261d693db9fc9593c430c3cf5d
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-15 23:58:42 -0300

    fix HeroSection

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -2,21 +2,22 @@ export default function HeroSection(){
   return(
     <section 
       id='inicio'
-      className="relative text-white min-h-screen flex items-center justify-center text-center 
-                 bg-gradient-to-b from-koda-blue-deep to-koda-blue-marino">
+      className="relative text-white min-h-screen flex items-center 
+                 bg-gradient-to-r from-koda-blue-marino to-koda-blue-deep">
         
-        <div className='px-4 flex flex-col items-center'> 
+        <div className='container mx-auto px-8 text-left'> 
 
-          <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4'>
+          <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4 max-w-2xl'>
             Software con <br /> calidad desde la raíz
           </h1>
          
-          <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
+          <p className="text-lg md:text-xl mb-8 max-w-xl">
             Desarrollamos software de calidad que impulsa tu negocio
           </p>
           
           <a href="#servicios"
-             className='bg-koda-green-emerald hover:bg-opacity-90 text-white font-bold py-3 px-8 rounded-lg text-lg transition-colors'>
+             className='inline-block text-white font-bold py-4 px-10 rounded-lg text-lg transition-all  bg-gradient-to-r from-koda-green-light to-koda-green-emerald
+                     hover:shadow-lg hover:shadow-koda-green-emerald/50'>
             Conoce nuestros servicios
           </a>
 
