366a725a167fc53ec9b0b9e7e9e21a08ce844df0
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-16 01:01:39 -0300

    Revert "fix herosection"
    This reverts commit 1f9ce7b0d2ec32473eae312b49ae6683e07142cc.

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -1,37 +1,28 @@
-export default function HeroSection() {
-  return (
-    <section
+export default function HeroSection(){
+  return(
+    <section 
       id='inicio'
-      
       className="relative text-white min-h-screen flex items-center 
-                 bg-gradient-to-br from-koda-blue-marino to-koda-blue-deep"
-    >
-      <div className='container mx-auto px-8 md:px-12 text-left'>
+                 bg-gradient-to-r from-koda-blue-marino to-koda-blue-deep">
         
-        <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4 max-w-3xl'>
-          Software con calidad desde la raíz
-        </h1>
-        
-        <p className="text-lg md:text-xl mb-8 max-w-xl opacity-90">
-          Desarrollamos software de calidad que impulsa tu negocio
-        </p>
-        
-      
-        <a 
-          href="#servicios"
-          className='
-            inline-block font-bold text-white text-lg rounded-lg transition-transform duration-200 
-            transform hover:-translate-y-1           
-            py-4 px-10 
-            bg-gradient-to-b from-koda-green-light to-koda-green-emerald 
-            border-b-4 border-koda-green-darker 
-            shadow-inner shadow-white/20
-          '
-        >
-          Conoce nuestros servicios
-        </a>
+        <div className='px-4 flex flex-col items-center'> 
 
-      </div>
+          <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4'>
+            Software con <br /> calidad desde la raíz
+          </h1>
+         
+          <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
+            Desarrollamos software de calidad que impulsa tu negocio
+          </p>
+          
+          <a href="#servicios"
+             className='inline-block text-white font-bold py-4 px-10 rounded-lg text-lg transition-all 
+                     bg-gradient-to-r from-koda-green-light to-koda-green-emerald
+                     hover:shadow-lg hover:shadow-koda-green-emerald/50'>
+            Conoce nuestros servicios
+          </a>
+
+        </div>
     </section>
   )
 }
