ecab739e9f82ae917f4b16a74c5809a0727aa53a
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-15 23:38:15 -0300

    fix HeroSection

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -1,19 +1,25 @@
-
 export default function HeroSection(){
   return(
     <section 
       id='inicio'
-     className="relative text-white min-h-screen flex items-center justify-center text-center 
+      className="relative text-white min-h-screen flex items-center justify-center text-center 
                  bg-gradient-to-b from-koda-blue-deep to-koda-blue-marino">
-        <div className='px-4'>
+        
+        <div className='px-4 flex flex-col items-center'> 
+
           <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4'>
             Software con <br /> calidad desde la raíz
           </h1>
-          <p>Desarrollamos software de calidad que impulsa tu negocio</p>
+         
+          <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
+            Desarrollamos software de calidad que impulsa tu negocio
+          </p>
+          
           <a href="#servicios"
-          className='bg-koda-green-emerald hover:bg-opacity-90 text-white font-bold py-3 px-8 rounded-lg text-lg transition-colors'>
+             className='bg-koda-green-emerald hover:bg-opacity-90 text-white font-bold py-3 px-8 rounded-lg text-lg transition-colors'>
             Conoce nuestros servicios
           </a>
+
         </div>
     </section>
   )
