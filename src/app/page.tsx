d30b7e733f298aefaa76f14c360321c064e1c2b2
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-15 23:05:21 -0300

    fix page +layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import HeroSection from './components/sections/HeroSection';
 import ContactSection from './components/sections/ContactoSections';
 import AboutSection from './components/sections/AboutSection';
-
+import ServicesSection from './components/sections/ServicesSection';
 
 export default function Home() {
   return (
     
-    <main className="p-8"> 
+    <> 
     <HeroSection/>
     <AboutSection/>
+    <ServicesSection/>
       <ContactSection/>
 
-    </main>
+    </>
   );
 }
