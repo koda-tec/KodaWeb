71bf6e2260904e058b06b33fa20705387f73b34c
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-17 00:21:21 -0300

    fix HeroSection

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -2,27 +2,31 @@ export default function HeroSection(){
   return(
     <section 
       id='inicio'
+      
+
       className="relative text-white min-h-screen flex items-center 
-                 bg-gradient-to-r from-koda-blue-marino to-koda-blue-deep">
+                 bg-gradient-to-r from-hero-start to-hero-end">
         
-        <div className='px-4 flex flex-col items-center'> 
+    
 
-          <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4'>
-            Software con <br /> calidad desde la raíz
-          </h1>
-         
-          <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
-            Desarrollamos software de calidad que impulsa tu negocio
-          </p>
-          
-          <a href="#servicios"
-             className='inline-block text-white font-bold py-4 px-10 rounded-lg text-lg transition-all 
-                     bg-gradient-to-r from-koda-green-light to-koda-green-emerald
-                     hover:shadow-lg hover:shadow-koda-green-emerald/50'>
-            Conoce nuestros servicios
-          </a>
+      <div className='container mx-auto px-8 md:px-12 text-left'> 
 
-        </div>
+        <h1 className='text-5xl md:text-7xl font-bold leading-tight mb-4 max-w-2xl'>
+          Software con calidad desde la raíz
+        </h1>
+       
+        <p className="text-lg md:text-xl mb-8 max-w-2xl opacity-90">
+          Desarrollamos software de calidad que impulsa tu negocio
+        </p>
+        
+       
+        <a href="#servicios"
+           className='inline-block text-white font-bold py-4 px-10 rounded-lg text-lg transition-transform hover:-translate-y-1 
+                      bg-gradient-to-b from-btn-start to-btn-end'>
+          Conoce nuestros servicios
+        </a>
+
+      </div>
     </section>
   )
 }
