b6ff47d499c198760c3fcde35c2f11f22512fe17
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-21 03:42:45 -0300

    fix filosofia

--- src/app/(pages)/filosofia/page.tsx
+++ src/app/(pages)/filosofia/page.tsx
@@ -1,32 +1,31 @@
-"use client"; 
+"use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y } from 'swiper/modules';
+import dynamic from "next/dynamic";
+import { A11y, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
+import { IntroSlide } from "@/app/components/about-slides/IntroSlide";
+import { HistorySlide } from "@/app/components/about-slides/HistorySlide";
+import { MissionVisionSlide } from "@/app/components/about-slides/MissionVisionSlide";
+import { DifferentiatorsSlide } from "@/app/components/about-slides/DifferentiatorsSlide";
+import { TeamSlide } from "@/app/components/about-slides/TeamSlide";
+import { CtaSlide } from "@/app/components/about-slides/CtaSlide";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-
-import { IntroSlide } from '@/app/components/about-slides/IntroSlide';
-import { HistorySlide } from '@/app/components/about-slides/HistorySlide';
-import { MissionVisionSlide } from '@/app/components/about-slides/MissionVisionSlide';
-import { DifferentiatorsSlide } from '@/app/components/about-slides/DifferentiatorsSlide';
-import { TeamSlide } from '@/app/components/about-slides/TeamSlide';
-import { CtaSlide } from '@/app/components/about-slides/CtaSlide';
+const Swiper = dynamic(() => import("swiper/react").then(mod => mod.Swiper), { ssr: false });
+const SwiperSlide = dynamic(() => import("swiper/react").then(mod => mod.SwiperSlide), { ssr: false });
 
 export default function AboutPage() {
   return (
     <div className="bg-white min-h-screen flex flex-col items-center justify-center py-12 px-4">
       <Swiper
-        
         modules={[Navigation, Pagination, A11y]}
         spaceBetween={50}
         slidesPerView={1}
-        navigation // Habilita las flechas de navegación
-        pagination={{ clickable: true }} // Habilita los puntos de paginación
-        className="w-full max-w-4xl" // Estilo del contenedor del carrusel
+        navigation
+        pagination={{ clickable: true }}
+        className="w-full max-w-4xl"
       >
         <SwiperSlide><IntroSlide /></SwiperSlide>
         <SwiperSlide><HistorySlide /></SwiperSlide>
