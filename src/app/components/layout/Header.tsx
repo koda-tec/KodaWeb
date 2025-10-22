dc9eded76e7a0eaa6463e2805e221a5471983730
Mili3312 <milagrosvillafane777@gmail.com>
2025-10-21 22:29:30 -0300

    fix

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -33,8 +33,8 @@ export default function Header() {
           <Image
             src="/logoKoda.png" 
             alt="Logo de KODA"
-            width={60}
-            height={60}
+            width={90}
+            height={90}
             className="w-10 h-10 md:w-12 md:h-12 object-contain transition-transform duration-300 hover:scale-105"
             priority
           />
